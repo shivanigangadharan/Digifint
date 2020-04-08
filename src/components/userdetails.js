80c3e24bb9947248257320cd7bb96571e13adb84
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-04-09 03:27:08 +0530

    user page frontend done

--- src/components/userdetails.js
+++ src/components/userdetails.js
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Form, Input, Icon, Button } from 'antd';
+import { Form, Icon } from 'antd';
 import { Link } from 'react-router-dom';
+import wallet5 from '../images/wallet5.jpg'
 
 const Box = styled.div`
-    box-shadow: 1px 1px 10px 3px lightgray;
-    width: 85%;
-    margin: 5%;
     padding: 2%;
+    width: 40%;
+    margin-top: 9%;
+
+`
+const Input = styled.input`
+    background: none;
+    border: 1px solid white;
+    font-size: 120%;
+    padding: 1%;
+    color: white;
+    margin-top: 3%;
+`
+const Button = styled.button`
+    background: none;
+    color: white;
+    font-size: 130%;
+    padding: 1.5%;
+    border: 1px solid white;
+    width: 40%;
+    margin-top: 2%;
+    :hover{
+        background: white;
+        opacity: 0.8;
+        color: black;
+        transition-duration: 1s;
+    }
+`
+const Main = styled.div`
+    background-image: url(${wallet5});
+    height: 100vh;
+    background-repeat: none;
+    background-position: center;
+    background-size: cover;
+    text-align:center;
+    padding-top: 1%;
 `
 function UserDetails() {
     const [acctname, setAcctName] = useState('');
@@ -17,18 +50,17 @@ function UserDetails() {
     const [branch, setBranch] = useState('');
     const [uniqueID, setUniqueId] = useState();
     return (
-        <div>user details page
+        <Main><center>
             <Box>
                 <Input onChange={e => { setAcctName(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Account holder name" />
-                <Input onChange={e => { setAcctNo(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Account Number" />
-                <Input onChange={e => { setBank(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Bank" />
-                <Input onChange={e => { setIfsc(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="IFSC Code" />
-                <Input onChange={e => { setBranch(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Branch name" />
-                <Button style={{ margin: '2%' }} htmlType="submit" type="primary"> Submit </Button>
-            </Box>
-            <Link to="/">
-                <Button> Back </Button></Link>
-        </div>
+                <br /> <Input onChange={e => { setAcctNo(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Account Number" />
+                <br /><Input onChange={e => { setBank(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Bank" />
+                <br /><Input onChange={e => { setIfsc(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="IFSC Code" />
+                <br /><Input onChange={e => { setBranch(e.target.value) }} style={{ margin: '1%', width: '70%', display: 'block' }} placeholder="Branch name" />
+                <br />  <Button style={{ margin: '2%' }} htmlType="submit" type="primary"> Submit </Button>
+            </Box></center>
+
+        </Main>
     )
 }
 
