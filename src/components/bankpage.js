38618eed55ec8e56463b6ecc2d3e2ef6b06ac2b3
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-04-09 10:03:22 +0530

    made bankpage frontend

--- src/components/bankpage.js
+++ src/components/bankpage.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import plant from '../images/plant.jpg'
 
 const Box = styled.div`
-    box-shadow: 1px 1px 10px 3px lightgray;
-    width: 85%;
-    margin: 5%;
+    box-shadow: 1px 1px 10px 3px darkgrey;
+    width: 40%;
     padding: 2%;
+    margin-top: 20vh;
+    margin-left: 53vw;
+
+`
+const Main = styled.div`
+    background-image: url(${plant});
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+    padding: 0.1%;
+`
+const Input = styled.input`
+    background: none;
+    border: 1px solid grey;
+    font-size: 120%;
+    padding: 1.5%;
+    margin-top: 8%;
+    color: black;
+`
+const Button = styled.button`
+background: none;
+border: 1px solid black;
+font-size: 125%;
+padding: 1.5%;
+margin-top: 8%;
 `
 function BankPage() {
     return (
-        <div>bank page
-        <Box>
-                <input placeholder="Enter unique id of user" /><br /><br />
-                <button> Submit </button> &emsp;
-            <Link to="/"><button> Back </button></Link>
+        <Main>
+            <Box>
+                <h2> Enter user details below </h2>
+                <Input placeholder="Enter unique id of user" /><br /><br />
+                <center><Button> Submit </Button></center>
             </Box>
-        </div>
+        </Main>
     )
 }
 
